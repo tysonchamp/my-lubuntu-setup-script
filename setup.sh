ae8794368c5e8f57737ab4f3f3d244c82519ce0c
Soumya Mondal <tysonchamp@users.noreply.github.com>
2018-12-22 19:48:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -55,6 +55,7 @@ echo "Installing Others Softwares"
 echo " "
 sudo apt-get install apt-transport-https
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text webhttrack libreoffice gparted vlc glipper -y
 echo "================================================================"
