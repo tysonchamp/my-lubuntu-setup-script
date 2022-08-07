4398ee67b7a4f7285728b6e8965a663b2185a228
Soumya Mondal <tysonchamp@users.noreply.github.com>
2022-08-07 14:10:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text webhttrack curl filezilla -y
+sudo snap install postman
 echo "================================================================"
 echo "Installing Nodejs Softwares"
 echo " "
