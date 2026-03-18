68206929bd2807f2e2ee86158e6fbca2e6121ea0
Soumya Mondal <tysonchamp@users.noreply.github.com>
2026-03-18 21:02:26 +0530

    Refactor Sublime Text installation in setup.sh
    Updated Sublime Text installation method to use keyrings and sources format.

--- setup.sh
+++ setup.sh
@@ -53,8 +53,8 @@ echo "================================================================"
 echo "Installing Others Softwares"
 echo " "
 sudo apt-get install apt-transport-https
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt-get update
 sudo apt-get install sublime-text webhttrack curl filezilla -y
 sudo snap install postman
